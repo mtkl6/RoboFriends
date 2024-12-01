20d7c24eb8e87094920a9935c5a2a3b88aeb5541
mtkl6 <166738040+mtkl6@users.noreply.github.com>
2024-12-01 21:16:09 +0100

    Revert "Implementet Hooks (useState, useEffect) in App.js."

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import CardList from "../components/CardList"
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import './App.css'
 
-function App()  {
-
-  const [robots, setRobots] = useState([])
-  const [searchField, setSearchField] = useState("")
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-        .then(users => setRobots(users));
-  }, [])
-
-  const onSearchChange = (event) => {
-    setSearchField({event.target.value})
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+        .then(users => this.setState({robots: users}));
+      }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  render() {
+    const {robots, searchField} = this.state;
     const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
@@ -27,13 +33,14 @@ function App()  {
     (
       <div className="tc">
       <h1 className="f1">RoboFriends</h1>
-      <SearchBox searchChange={onSearchChange}/>
+      <SearchBox searchChange={this.onSearchChange}/>
       <Scroll>
       <CardList robots={filteredRobots}/>
       </Scroll>
       </div>
     );
   }
+};
 
 
 export default App;
